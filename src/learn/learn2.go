d8e1180f2cc879febb3e1dd9024927021da50e5b
ebiz <lustriaebiz@gmail.com>
2018-04-29 23:29:30 +0700

    learn2
    variabel

---
+++ src/learn/learn2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var firstName string = "Ebiz"
+
+    var lastName string
+    lastName = "Lustria"
+
+    fmt.Printf("halo %s %s!\n", firstName, lastName)
+}
