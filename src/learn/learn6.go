93eaa6aa1aa6fcda00bfe67d133be9e4aec37c0c
ebiz <lustriaebiz@gmail.com>
2018-04-29 23:48:08 +0700

    learn6
    array

---
+++ src/learn/learn6.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [4]string
+	names[0] = "mangga"
+	names[1] = "jambu"
+	names[2] = "anggur"
+	names[3] = "jeruk"
+	
+	fmt.Println(names[0], names[1], names[2], names[3])
+}
