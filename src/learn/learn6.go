14c4d3bc14b6cbdb619ffc7df5f403be2798573d
ebiz <lustriaebiz@gmail.com>
2018-04-29 23:49:18 +0700

    learn6
    Inisialisasi Nilai Awal Array

--- src/learn/learn6.go
+++ src/learn/learn6.go
@@ -10,4 +10,9 @@ func main() {
 	names[3] = "jeruk"
 	
 	fmt.Println(names[0], names[1], names[2], names[3])
+
+	var fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+	fmt.Println("Jumlah element \t\t", len(fruits))
+	fmt.Println("Isi semua element \t", fruits)
 }
